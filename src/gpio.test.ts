7f7e6f5bdabe8c706b6ac14b4fe2e57512832b7f
Сергей Чернов <chernovsi@m2.ru>
2022-04-03 18:00:42 +0300

    reconfigureDirection

--- src/gpio.test.ts
+++ src/gpio.test.ts
@@ -6,3 +6,13 @@ test('Initial', () => {
   pin2.writeSync(1);
   expect(pin2.readSync()).toBe(1);
 });
+
+test('reconfigureDirection', () => {
+    const pin21 = new Gpio(2, 'out');
+    pin21.writeSync(1);
+    const pin22 = new Gpio(2, 'out');
+    expect(pin22.readSync()).toBe(0);
+    pin22.writeSync(1);
+    const pin23 = new Gpio(2, 'out', 'both', {reconfigureDirection: false});
+    expect(pin23.readSync()).toBe(1);
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
-import { BinaryValue, Edge, Gpio as GpioOriginal } from 'onoff';
+import { BinaryValue, Edge, Gpio as GpioOriginal, Options } from 'onoff';
+
+import {tmpdir} from 'os';
+
+import { existsSync, writeFileSync, readFileSync } from 'fs';
+
+const TMP_FILE = tmpdir()+'/gpio_debug.json';
 class Gpio {
-  static states = [0, 0, 0, 0, 0];
+  static states:Array<number> = new Array<number>(40);
 
   private gpio = 0;
   private original: GpioOriginal | undefined = undefined;
-  constructor(gpio: number, direction: any, edge: Edge = 'none', options = {}) {
+  constructor(gpio: number, direction: any, edge: Edge = 'none', options:Options = {}) {
     try {
       this.original = new GpioOriginal(gpio, direction, edge, options);
     } catch (e) {
       console.info('The Debug Mode is on');
       this.gpio = gpio;
+      if(!existsSync(TMP_FILE)){
+        writeFileSync(TMP_FILE, JSON.stringify(Gpio.states.fill(0)));
+      }
+      Gpio.states = JSON.parse(readFileSync(TMP_FILE, 'utf8'));
+      if(options.reconfigureDirection !== false){
+        Gpio.states[this.gpio] = 0;
+        writeFileSync(TMP_FILE, JSON.stringify(Gpio.states));
+      }
+      
     }
   }
   readSync() {
@@ -23,6 +38,7 @@ class Gpio {
       return this.original.writeSync(value);
     }
     Gpio.states[this.gpio] = value === 0 ? 0 : 1;
+    writeFileSync(TMP_FILE, JSON.stringify(Gpio.states));
   }
 }
 
