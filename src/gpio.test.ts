f1fc6da7b89b3725df70fec3ab7ade5dea93b861
Сергей Чернов <chernovsi@m2.ru>
2022-04-03 18:02:08 +0300

    1.1.0

--- src/gpio.test.ts
+++ src/gpio.test.ts
@@ -8,11 +8,11 @@ test('Initial', () => {
 });
 
 test('reconfigureDirection', () => {
-    const pin21 = new Gpio(2, 'out');
-    pin21.writeSync(1);
-    const pin22 = new Gpio(2, 'out');
-    expect(pin22.readSync()).toBe(0);
-    pin22.writeSync(1);
-    const pin23 = new Gpio(2, 'out', 'both', {reconfigureDirection: false});
-    expect(pin23.readSync()).toBe(1);
-  });
+  const pin21 = new Gpio(2, 'out');
+  pin21.writeSync(1);
+  const pin22 = new Gpio(2, 'out');
+  expect(pin22.readSync()).toBe(0);
+  pin22.writeSync(1);
+  const pin23 = new Gpio(2, 'out', 'both', { reconfigureDirection: false });
+  expect(pin23.readSync()).toBe(1);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
 import { BinaryValue, Edge, Gpio as GpioOriginal, Options } from 'onoff';
 
-import {tmpdir} from 'os';
+import { tmpdir } from 'os';
 
 import { existsSync, writeFileSync, readFileSync } from 'fs';
 
-const TMP_FILE = tmpdir()+'/gpio_debug.json';
+const TMP_FILE = tmpdir() + '/gpio_debug.json';
 class Gpio {
-  static states:number[] = new Array<number>(40);
+  static states: number[] = new Array<number>(40);
 
   private gpio = 0;
   private original: GpioOriginal | undefined = undefined;
-  constructor(gpio: number, direction: any, edge: Edge = 'none', options:Options = {}) {
+  constructor(gpio: number, direction: any, edge: Edge = 'none', options: Options = {}) {
     try {
       this.original = new GpioOriginal(gpio, direction, edge, options);
     } catch (e) {
       console.info('The Debug Mode is on');
       this.gpio = gpio;
-      if(!existsSync(TMP_FILE)){
+      if (!existsSync(TMP_FILE)) {
         writeFileSync(TMP_FILE, JSON.stringify(Gpio.states.fill(0)));
       }
       Gpio.states = JSON.parse(readFileSync(TMP_FILE, 'utf8'));
-      if(options.reconfigureDirection !== false){
+      if (options.reconfigureDirection !== false) {
         Gpio.states[this.gpio] = 0;
         writeFileSync(TMP_FILE, JSON.stringify(Gpio.states));
       }
-      
     }
   }
   readSync() {
